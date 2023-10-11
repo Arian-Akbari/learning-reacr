439bb2670dfa8fcdf37e246ba878bdf79caa037f
“Arian” <arian09337332108@gmail.com>
2023-10-11 09:28:56 +0330

    trying to work on .map() in react

--- src/App.js
+++ src/App.js
@@ -2,16 +2,17 @@ import Image from "./project 1/Image";
 import Introduction from "./project 1/intoduction";
 import Buttons from "./project 1/Buttons";
 import About from "./project 1/About";
+import Practice from "./Practice";
 
 export default function App(){
+    const colors = ["red" , "blue" , "yellow"];
+    const el = colors.map((color) =>{
+        return <Practice color = {color}/>
+    })
+
   return(
-          <div className={"main"}>
-              <div className={"top"}>
-                  <Image />
-                  <Introduction />
-                  <About />
-                  <Buttons />
-              </div>
+          <div>
+              {el}
           </div>
       )
 }
---
+++ src/Practice.js
@@ -0,0 +1,9 @@
+export default function Practice(props){
+    return(
+        <div>
+            <h1>
+                {props.color}
+            </h1>
+        </div>
+    )
+}
