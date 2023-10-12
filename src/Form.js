1030ca53052a0de6434f91ad33a3d9b39b00e254
“Arian” <arian09337332108@gmail.com>
2023-10-12 12:13:00 +0330

    finsihed the memeGenerator and added soo many new thinggs

--- src/Form.js
+++ src/Form.js
@@ -9,9 +9,10 @@ export default function App() {
             comment:"",
             isFriend: true,
             employment: "",
+            favColor:""
         }
     )
-    console.log(formData.employment)
+    console.log(formData)
     function handleChange(event){
         setformData(prevState => {
             const {name , value , type , checked} = event.target
@@ -22,81 +23,105 @@ export default function App() {
         })
     }
 
+    function handleSubmit(event){
+        event.preventDefault()
+
+
+    }
 
     return (
         <div className={"main"}>
-            <input
-                type={"text"}
-                placeholder={"First Name"}
-                onChange={handleChange}
-                name={"firstName"}
-                value={formData.firstName}
-            />
-            <input
-                type={"text"}
-                placeholder={"First Name"}
-                onChange={handleChange}
-                name={"lastName"}
-                value={formData.lastName}
-            />
-            <input
-                type={"email"}
-                placeholder={"Email"}
-                onChange={handleChange}
-                name={"email"}
-                value={formData.email}
-            />
-            <textarea
-                value={formData.comment}
-                placeholder={"comment"}
-                onChange={handleChange}
-                name={"comment"}
-            />
-            <input
-                type={"checkbox"}
-                id={"friend"}
-                checked={formData.isFriend}
-                onChange={handleChange}
-                name={"isFriend"}
-            />
-            <label htmlFor={"friend"}>are we friends ?</label>
-            <br/>
-            <br/>
-            <fieldset>
-                <legend>current employment status</legend>
+            <form onSubmit={handleSubmit}>
                 <input
-                    type={"radio"}
-                    id={"unemployed"}
-                    name={"employment"}
-                    value={"unemployed"}
+                    type={"text"}
+                    placeholder={"First Name"}
                     onChange={handleChange}
-                    checked={formData.employment === "unemployed"}
+                    name={"firstName"}
+                    value={formData.firstName}
                 />
-                <label htmlFor={"unemployed"}>UnEmployed</label>
+                <input
+                    type={"text"}
+                    placeholder={"First Name"}
+                    onChange={handleChange}
+                    name={"lastName"}
+                    value={formData.lastName}
+                />
+                <input
+                    type={"email"}
+                    placeholder={"Email"}
+                    onChange={handleChange}
+                    name={"email"}
+                    value={formData.email}
+                />
+                <textarea
+                    value={formData.comment}
+                    placeholder={"comment"}
+                    onChange={handleChange}
+                    name={"comment"}
+                />
+                <input
+                    type={"checkbox"}
+                    id={"friend"}
+                    checked={formData.isFriend}
+                    onChange={handleChange}
+                    name={"isFriend"}
+                />
+                <label htmlFor={"friend"}>are we friends ?</label>
                 <br/>
+                <br/>
+                <fieldset>
+                    <legend>current employment status</legend>
+                    <input
+                        type={"radio"}
+                        id={"unemployed"}
+                        name={"employment"}
+                        value={"unemployed"}
+                        onChange={handleChange}
+                        checked={formData.employment === "unemployed"}
+                    />
+                    <label htmlFor={"unemployed"}>UnEmployed</label>
+                    <br/>
 
-                <input
-                    type={"radio"}
-                    id={"fullTime"}
-                    name={"employment"}
-                    value={"fullTime"}
-                    onChange={handleChange}
-                    checked={formData.employment === "fullTime"}
-                />
-                <label htmlFor={"fullTime"}>FullTime</label>
-                <br/>
+                    <input
+                        type={"radio"}
+                        id={"fullTime"}
+                        name={"employment"}
+                        value={"fullTime"}
+                        onChange={handleChange}
+                        checked={formData.employment === "fullTime"}
+                    />
+                    <label htmlFor={"fullTime"}>FullTime</label>
+                    <br/>
 
-                <input
-                    type={"radio"}
-                    id={"partTime"}
-                    name={"employment"}
-                    value={"partTime"}
-                    onChange={handleChange}
-                    checked={formData.employment === "partTime"}
-                />
-                <label htmlFor={"partTime"}>PartTime</label>
+                    <input
+                        type={"radio"}
+                        id={"partTime"}
+                        name={"employment"}
+                        value={"partTime"}
+                        onChange={handleChange}
+                        checked={formData.employment === "partTime"}
+                    />
+                    <label htmlFor={"partTime"}>PartTime</label>
+                    <br/>
+                </fieldset>
+
+                <label htmlFor={"favColor"} >what is your favorite color ?</label>
                 <br/>
-            </fieldset>
+                <select
+                    id={"favColor"}
+                    value={formData.favColor}
+                    onChange={handleChange}
+                    name={"favColor"}
+                >
+                    <option value={""}>--chose--</option>
+                    <option value={"red"}>Red</option>
+                    <option value={"orange"}>Orange</option>
+                    <option value={"blue"}>blue</option>
+                </select>
+                <br/>
+                <br/>
+                <button>Submit</button>
+            </form>
 
         </div>
     )
--- src/MemeGenerator/Meme.js
+++ src/MemeGenerator/Meme.js
@@ -1,16 +1,33 @@
 import MemesData from "./MemesData";
-import React, {useState} from "react";
-
+import {useEffect, useState} from "react";
 export default function Meme() {
-    const [memeImage , setMemeImage] = useState("")
-    const [testing , setTesting] = useState(true)
-    function getMemeImage() {
-        const meme = MemesData
-        const randomNumber = Math.floor(Math.random() * meme.length)
-        setMemeImage(meme[randomNumber])
-        setTesting(prevState => !prevState)
-        console.log(testing)
+    const [memeImage , setMemeImage] = useState({
+        topText:"",
+        bottomText:"",
+        randomImage: "http://i.imgflip.com/1bij.jpg"
+    })
+    const [allMemes , setAllMemes] = useState([])
 
+    useEffect(() =>{
+        fetch("https://api.imgflip.com/get_memes")
+            .then(res => res.json())
+            .then(data => setAllMemes(data.data.memes))
+    } , [])
+
+    function getMemeImage() {
+        const randomArray = Math.floor(Math.random() * allMemes.length)
+        const url = allMemes[randomArray].url
+        setMemeImage(prevState => ({
+            ...prevState,
+            randomImage: url
+        }))
+    }
+    function handleChange (event){
+        const {name , value} = event.target
+        setMemeImage(prevState => ({
+            ...prevState,
+            [name]:value
+        }))
     }
 
     return (
@@ -20,17 +37,29 @@ export default function Meme() {
                     type={"text"}
                     className={"form--input"}
                     placeholder={"Top Text"}
+                    name={"topText"}
+                    value={memeImage.topText}
+                    onChange={handleChange}
                 />
                 <input
                     type={"text"}
                     className={"form--input"}
                     placeholder={"Bottom Text"}
+                    name={"bottomText"}
+                    value={memeImage.bottomText}
+                    onChange={handleChange}
                 />
                 <button onClick={getMemeImage} className={"form--button"}>
                     Get a new meme image
                 </button>
             </div>
-            <img src={memeImage} className={"meme--image"}/>
+            <div className={"container"}>
+                <img src={memeImage.randomImage} className={"meme--image"} />
+                <p className={"top_text text-overlay"}>{memeImage.topText}</p>
+                <p className={"bottom_text text-overlay"}>{memeImage.bottomText}</p>
+            </div>
+
         </main>
     )
 }
+
